a71241302648bb62b6cb33429025729eb0d7aa25
kingdev1026 <kingdev10260522@gmail.com>
2022-01-07 15:38:59 +0900

    finish tab-header commit

--- src/components/tablayout/TabLayout.js
+++ src/components/tablayout/TabLayout.js
@@ -8,18 +8,19 @@ import {FaRegGrin} from "react-icons/fa";
 
 function TabLayout () {
     return(
-        <div className="TabLayout">
-            <Container>
-                <Tabs defaultActiveKey="musicians" id="uncontrolled-tab-example" className="mb-3">
+        <Container>
+            <div className="TabLayout">
+                <Tabs defaultActiveKey="musicians" id="uncontrolled-tab-example">
                     <Tab eventKey="musicians" title={<span><FaMusic className="tab-icon" />  Musicians </span>}>
+                    asdfasdfasdfasfafsdfasfdasdfasdfasdfasfafsdfasfdasdfasdfasdfasfafsdfasfd
                     </Tab>
                     <Tab eventKey="dj" title={<span><FaRegDotCircle className="tab-icon" />  DJ </span>}>
                     </Tab>
                     <Tab eventKey="comedian" title={<span><FaRegGrin className="tab-icon" />  Comedian </span>}>
                     </Tab>
                 </Tabs>
-            </Container>
-        </div>
+            </div>
+        </Container>
     )
 }
 
