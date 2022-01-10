925c3b31bcc336f5278088e6b2215a4a87765dec
kingdev1026 <kingdev10260522@gmail.com>
2022-01-10 21:33:20 +0300

    result list finish commit

--- src/components/resultbody/ResultBody.js
+++ src/components/resultbody/ResultBody.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import './ResultBody.css';
-import { Container, Row, Col, Button, ButtonGroup } from "react-bootstrap";
+import { Container, Row, Col, Button, ButtonGroup, Card } from "react-bootstrap";
 import { BsFunnelFill } from "react-icons/bs";
 import ReactStars from 'react-stars';
 import { Slider, Rail, Handles, Tracks } from 'react-compound-slider'
@@ -8,17 +8,17 @@ import { Slider, Rail, Handles, Tracks } from 'react-compound-slider'
 const sliderStyle = {  // Give the slider some width
     position: 'relative',
     width: '100%',
-    height: 80,
-    backgroundColor: 'white',
+    height: 40,
+    // backgroundColor: 'white',
     // border: '1px solid steelblue',
 }  
 const railStyle = {
     position: 'absolute',
     width: '100%',
     height: 10,
-    marginTop: 35,
+    marginTop: 15,
     borderRadius: 5,
-    backgroundColor: '#8B9CB6',
+    backgroundColor: '#f1f1f1',
 }
 const Handle = ({
     handle: { id, value, percent },
@@ -29,23 +29,23 @@ const Handle = ({
           style={{
             left: `${percent}%`,
             position: 'absolute',
-            marginLeft: -10,
-            marginTop: 25,
+            marginLeft: -5,
+            marginTop: 7,
             zIndex: 2,
             width: 15,
-            height: 30,
+            height: 25,
             border: 0,
             textAlign: 'center',
             cursor: 'pointer',
-            // borderRadius: '50%',
-            backgroundColor: '#2C4870',
+            borderRadius: '20%',
+            backgroundColor: '#2a917d',
             color: '#333',
           }}
           {...getHandleProps(id)}
         >
-          <div style={{ fontFamily: 'Roboto', fontSize: 11, marginTop: -35 }}>
+          {/* <div style={{ fontFamily: 'Roboto', fontSize: 11, marginTop: -35 }}>
             {value}
-          </div>
+          </div> */}
         </div>
     )
 }
@@ -56,8 +56,8 @@ const Track = ({source, target, getTrackProps}) => {
                 position: 'absolute',
                 height: 10,
                 zIndex: 1,
-                marginTop: 35,
-                backgroundColor: '#546C91',
+                marginTop: 15,
+                backgroundColor: '#2a917d',
                 borderRadius: 5,
                 cursor: 'pointer',
                 left: `${source.percent}%`,
@@ -68,53 +68,86 @@ const Track = ({source, target, getTrackProps}) => {
     )
 }
 const CustomSlider = (props) => {
+    const [curVal, setCurVal] = useState(props.minval + props.unit + " - " + props.maxval + props.unit)
     return(
-        <Slider
-            rootStyle={sliderStyle /* inline styles for the outer div. Can also use className prop. */}
-            domain={[0, 100]}
-            step={1}
-            mode={2}
-            values={[0, 100]}
-        >
-            <Rail>
-                {({ getRailProps }) => (
-                    <div style={railStyle} {...getRailProps()} />
+        <div className="CustomSlider">
+            <div className="Slider-Text">
+                <div className="Slider-Title Title-Class">{props.title}</div>
+                <div className="Slider-Curval">{curVal}</div>
+            </div>
+            <Slider
+                rootStyle={sliderStyle /* inline styles for the outer div. Can also use className prop. */}
+                domain={[props.minval, props.maxval]}
+                step={1}
+                mode={2}
+                values={[props.minval, props.maxval]}
+                onChange={(event) => {setCurVal(event[0] + props.unit + " - " + event[1] + props.unit) }}
+                className="Slider-Main"
+            >
+                <Rail>
+                    {({ getRailProps }) => (
+                        <div style={railStyle} {...getRailProps()} />
+                    )}
+                </Rail>
+                <Handles>
+                    {({ handles, getHandleProps }) => (
+                        <div className="slider-handles">
+                            {handles.map(handle => (
+                                <Handle
+                                    key={handle.id}
+                                    handle={handle}
+                                    getHandleProps={getHandleProps}
+                                />
+                            ))
+                            }
+                        </div>
+                    )}
+                </Handles>
+                <Tracks right={false} left={false}>
+                {({ tracks, getTrackProps }) => (
+                    <div className="slider-tracks">
+                    {tracks.map(({ id, source, target }) => (
+                        <Track
+                            key={id}
+                            source={source}
+                            target={target}
+                            getTrackProps={getTrackProps}
+                        />
+                    ))}
+                    </div>
                 )}
-            </Rail>
-            <Handles>
-            {({ handles, getHandleProps }) => (
-                <div className="slider-handles">
-                {handles.map(handle => (
-                    <Handle
-                    key={handle.id}
-                    handle={handle}
-                    getHandleProps={getHandleProps}
-                    />
-                ))}
-                </div>
-            )}
-            </Handles>
-            <Tracks right={false}>
-              {({ tracks, getTrackProps }) => (
-                <div className="slider-tracks">
-                {tracks.map(({ id, source, target }) => (
-                    <Track
-                    key={id}
-                    source={source}
-                    target={target}
-                    getTrackProps={getTrackProps}
-                    />
-                ))}
-                </div>
-            )}
-            </Tracks>
-        </Slider>
+                </Tracks>
+            </Slider>
+        </div>
+    )
+}
+const CustomCheck = (props) => {
+    return (
+        <div class="CustomCheck">
+            <input type="checkbox" class="Check-Control-Input" id="Check-Control-Input" indeterminate />
+            <span class="checkmark"></span>
+            <label class="Check-Control-Label" for="Check-Control-Input">{props.title}</label>
+      </div>
+    )
+}
+const CustomCard = (props) => {
+    return (
+        <div className="CustomCard">
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 const RatingStar = (props) => {
-    // const ratingChanged = (newRating) => {
-    //     setRate(newRating)
-    // }
     const [rate, setRate] = useState(5)
     return (
         <div className="RatingStar">
@@ -143,18 +176,18 @@ function ResultBody () {
                     <Col lg={3} sm={12} className="Search">
                         <div className="Search-Title">
                             <BsFunnelFill className="Search-Icon"/>
-                            <div className="Search-Title1">
+                            <div className="Search-Title1 Title-Class">
                                 Suche eingrenzen
                             </div>
                         </div>
                         <div className="Bewertung">
-                            <div className="Bewertung-Title">
+                            <div className="Bewertung-Title Title-Class">
                                 Bewertung
                             </div>
                             <RatingStar title="mind." value={0} />
                         </div>
                         <div className="Geschiecht">
-                            <div className="Geschiecht-Title">
+                            <div className="Geschiecht-Title Title-Class">
                                 Geschiecht
                             </div>
                             <ButtonGroup className="Geschiecht-Button" aria-label="Basic example">
@@ -163,7 +196,34 @@ function ResultBody () {
                                 <Button className="Every-Button" variant="secondary ">egal</Button>
                             </ButtonGroup>
                         </div>
-                        <CustomSlider />
+                        <CustomSlider title="Preis" minval={470} maxval={2260} unit="€"/>
+                        <CustomSlider title="Entfernung" minval={0} maxval={300} unit="km"/>
+                        <CustomSlider title="Alter" minval={25} maxval={74} unit="Jahre"/>
+                        <CustomSlider title="Erfahrung" minval={3} maxval={57} unit="Jahre"/>
+                        <CustomSlider title="Bewertungen" minval={0} maxval={307} unit="Jahre"/>
+                        <CustomSlider title="Buchungen" minval={0} maxval={602} unit="Jahre"/>
+                        <div className="Musik-Rich">
+                            <div className="Musik-Title Title-Class">
+                                Musikrichtungen
+                            </div>
+                            <CustomCheck title="80er Hits"/>
+                            <CustomCheck title="90er Hits"/>
+                            <CustomCheck title="Allround"/>
+                            <CustomCheck title="Charts"/>
+                            <CustomCheck title="Country"/>
+                            <CustomCheck title="Disco"/>
+                            <CustomCheck title="Eloktro"/>
+                            <CustomCheck title="Funk"/>
+                            <CustomCheck title="Hip Hop"/>
+                            <CustomCheck title="House"/>
+                        </div>
+                    </Col>
+                    <Col lg={9} sm={12}>
+                        <Row>
+                            <Col lg={4} sm={6}>
+                                <CustomCard />
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
